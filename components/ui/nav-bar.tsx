6ae7de67a6886fbd792813c08ce0f82c800e0625
Brunhilde <brandontu35@gmail.com>
2024-05-15 18:38:10 -0400

    feat added nav bar for now

--- components/ui/nav-bar.tsx
+++ components/ui/nav-bar.tsx
@@ -2,21 +2,16 @@
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import navbar from "@/components/ui/nav-bar"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+export default function navbar() {
+    return(
+        <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
         <div className="flex w-full justify-center">
           <NavigationMenu className="hidden lg:flex">
             <NavigationMenuList>
@@ -52,18 +47,6 @@ export default function Home() {
             </NavigationMenuList>
           </NavigationMenu>
         </div>
-
       </header>
-
-      <h1>nav bar goes here</h1>
-      <h1>About me </h1>
-      <h1>Project carousel</h1>
-      <h1>work history timeline</h1>
-      <h1>resume</h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
-
-    </main>
-  );
+    )
 }
